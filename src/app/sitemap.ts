43daa436aa617a4b9f37a2647f3551a3d4468eb7
compumaxhosting <compumaxhost@gmail.com>
2026-03-13 20:18:02 +0530

    zzzz

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://yourdomain.com";
+  const baseUrl = "http://topcoat-llc.com/";
 
   return [
     {
